16a7ced4993bcadff4e3df18db8dabc2b3c2fee4
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-28 14:13:46 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,11 +6,11 @@ def third_challenge
         :age => "53"
         },
       :matriarch => {
-        name => "Lady Montague",
+        :name => "Lady Montague",
         :age => "54"
         },
       :hero => {
-        name => "Romeo",
+        :name => "Romeo",
         :age => "15",
         :status => "alive"
         },
@@ -18,15 +18,15 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        name => "Lord Capulet",
+        :name => "Lord Capulet",
         :age => "50"
         },
       :matriarch => {
-        name => "Lady Capulet",
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
-        name => "Juliet",
+        :name => "Juliet",
         :age => "15"
         },
       :heroine_friends => []
